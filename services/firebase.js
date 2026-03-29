3d281ea7c1768620df839859e0833e70c5565709
NisargKaneriya-Prioxis <Nisarg@prioxis.com>
2026-03-29 17:15:06 +0530

    Load firebase credentials from env variables

--- services/firebase.js
+++ services/firebase.js
@@ -1,8 +1,26 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../healthsync-5cfb1-firebase-adminsdk-fbsvc-9b77f5c8e3"); // your downloaded file
+// const admin = require("firebase-admin");
+// const serviceAccount = require("../healthsync-5cfb1-firebase-adminsdk-fbsvc-9b77f5c8e3"); // your downloaded file
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// module.exports = admin;
+const admin = require('firebase-admin');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
